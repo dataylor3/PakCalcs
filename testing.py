c0d7be509ad4412af63df8d4bf825c0f3a3a0ae8
dataylor3 <david.taylor3@jacobs.com>
2026-03-19 12:58:30 +1100

    plotting in progress

---
+++ testing.py
@@ -0,0 +1,34 @@
+import forallpeople as si
+import pandas as pd
+# Load SI base + derived units
+si.environment("mystructural", top_level=True)
+from handcalcs.decorator import handcalc
+#import handcalcs.render
+from math import cos, sin, radians, degrees
+g_acc = 9.81*m/s**2  # gravitational acceleration
+from SG_file_parser import sgFileParser
+from design_member import DesignMember
+from member_design_actions import MemberDesignActions
+from pprint import pprint
+from member_plotter import MemberPlotter
+
+results = sgFileParser(r"C:\Users\DATaylor\Documents\Personal\PakCalcs\PakCalcs\3d Frame 260317.TXT")
+
+rafter1 = DesignMember.build_many(results.design_members)
+#print((list(rafter1)))
+
+des_act = MemberDesignActions(list(rafter1[0].element_list), [11,12,13,14,15], results.design_actions_parsed)
+pprint(des_act.globalized)
+
+action_units = {
+    "Ax": "N",
+    "Vy": "N",
+    "Vz": "N",
+    "Mx": "N·m",
+    "My": "N·m",
+    "Mz": "N·m",
+}
+plotter = MemberPlotter(des_act.globalized, action_units=action_units)
+
+fig = plotter.plot_3d(action="Mz")
+fig.show()
