77d4b30a19450abaf513fc3d0e872d741f32b42e
dataylor <dathome1@gmail.com>
2026-03-24 20:50:49 +1100

    contemplate create design member

--- testing.py
+++ testing.py
@@ -11,25 +11,27 @@ from design_member import DesignMember
 from member_design_actions import MemberDesignActions
 from pprint import pprint
 from member_plotter import MemberPlotter
+from timberClasses import beamDesign
 
 #results = sgFileParser(r"C:\Users\DATaylor\Documents\Personal\PakCalcs\PakCalcs\3d Frame 260317.TXT")
 results = sgFileParser(r"3d Frame 260317.TXT")
+#print(results.units)
 
-rafter1 = DesignMember.build_many(results.design_members)
-#print((list(rafter1)))
+members = DesignMember.build_many(results.design_members)
+#print((list(members[0])))
 
-des_act = MemberDesignActions(list(rafter1[0].element_list), [11,12,13,14,15,16], results.design_actions_parsed)
-#pprint(des_act.globalized)
+des_act = MemberDesignActions(list(members[1].element_list), [11], results.design_actions_parsed)
+pprint(des_act.globalized)
 
 action_units = {
-    "Ax": "kN",
-    "Vy": "kN",
-    "Vz": "kN",
-    "Mx": "kN·m",
-    "My": "kN·m",
-    "Mz": "kN·m",
+    "Ax": results.units.get("FORCE"),
+    "Vy": results.units.get("FORCE"),
+    "Vz": results.units.get("FORCE"),
+    "Mx": results.units.get("MOMENT"),
+    "My": results.units.get("MOMENT"),
+    "Mz": results.units.get("MOMENT"),
 }
 plotter = MemberPlotter(des_act.globalized, action_units=action_units)
 
-fig = plotter.plot_3d(action="Mz")
+fig = plotter.plot_3d(action="Vz")
 fig.show()
